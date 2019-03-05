47e6825dd52025cfb41a55988c27c6f0ee5278c7
Giorgio Zanni <g.zanni@hotmail.com>
2019-03-05 14:52:28 -0300

    ADD: add name to devise form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :portfolios
+  has_many :portfolios, dependent: :destroy
   has_many :coins, through: :portfolios
-  has_many :comments
+  has_many :comments, dependent: :destroy
+
+  validates :name, presence: true
 
   enum role: %i[visit user moderator]
 end
