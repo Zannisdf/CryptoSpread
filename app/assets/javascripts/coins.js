77939dc3f68727a22cdec83edef12ae59a30a22f
Giorgio Zanni <g.zanni@hotmail.com>
2019-03-17 22:04:18 -0300

    ADD: style to main views

--- app/assets/javascripts/coins.js
+++ app/assets/javascripts/coins.js
@@ -12,4 +12,15 @@ $(document).on('turbolinks:load', function(){
     sortList: [[2,1]],
     widgets: ['zebra']
   })
+
+  checkPrice($('.price'));
 });
+
+function checkPrice(priceObj){
+  price = priceObj.text().split('').slice(0,priceObj.text().indexOf('%')).join('');
+  if(price > 0){
+    priceObj.addClass('positive');
+  } else if(price < 0){
+    priceObj.addClass('negative');
+  }
+}
